Test on CIFAR10 generation over different decoder. Fix the logits bug, different sign bug and setting name bug.

attack.py
@@ -75,15 +75,15 @@ TRAINING_IMAGE_SHAPE = settings.config["IMAGE_SHAPE"]
 EPOCHS = 4
 EPSILON = 1e-5
 BATCH_SIZE = settings.config["BATCH_SIZE"]
-if data_set=="cifar10":
+if data_set == "cifar10":
     LEARNING_RATE = 1e-2
     LR_DECAY_RATE = 1e-4
     DECAY_STEPS = 1.0
-    adv_weight = 5000
-    ITER=500
+    adv_weight = 500
+    ITER=2000
     CLIP_NORM_VALUE = 10.0
 else:
-    if model_name == "imagenet_shallowest":
+    if model_name .find("shallowest")>=0:
         LEARNING_RATE = 5e-3
     else:
         LEARNING_RATE = 1e-2
@@ -106,7 +106,7 @@ def grad_attack():
     sess.run(stn.init_style, feed_dict=fdict)
     sess.run(global_step.initializer)
     rst_img, rst_loss, nat_acc, rst_acc,rst_mean,rst_sigma = sess.run(
-        [adv_img, content_loss_y, nat_output.acc_y_5, adv_output.acc_y_5, stn.meanS, stn.sigmaS],  feed_dict=fdict)
+        [adv_img, content_loss_y, nat_output.acc_y_auto, adv_output.acc_y_auto, stn.meanS, stn.sigmaS],  feed_dict=fdict)
     print("Nature Acc:", nat_acc)
     for i in range(ITER):
         # Run an optimization step
@@ -117,7 +117,7 @@ def grad_attack():
         
         # Monitor the progress
         _adv_img, acc, aloss, closs, _mean, _sigma = sess.run(
-            [adv_img, adv_output.acc_y_5, adv_loss, content_loss_y, stn.meanS, stn.sigmaS],  feed_dict=fdict)
+            [adv_img, adv_output.acc_y_auto, adv_loss, content_loss_y, stn.meanS, stn.sigmaS],  feed_dict=fdict)
         for j in range(BATCH_SIZE):
             # Save the best samples
             if acc[j]<rst_acc[j] or (acc[j]==rst_acc[j] and closs[j]<rst_loss[j]): 
@@ -179,7 +179,7 @@ with tf.Graph().as_default(), tf.Session(config=tf_config) as sess:
     target_features = stn.target_features
 
     # pass the generated_img to the encoder, and use the output compute loss
-    enc_gen_adv, enc_gen_layers_adv = stn.encoder.encode(adv_img)
+    enc_gen_adv, enc_gen_layers_adv = stn.encode(adv_img)
     
     modelprep.init_classifier()
     build_model = modelprep.build_model
@@ -194,7 +194,7 @@ with tf.Graph().as_default(), tf.Session(config=tf_config) as sess:
     # We are minimizing the loss. Take the negative of the loss
     # Use CW loss top5 for imagenet and CW top1 for cifar10. 
     # Here the target_loss represents CW loss, it is not the loss for targeted attack.
-    adv_loss = - adv_output.target_loss_auto
+    adv_loss = -adv_output.target_loss_auto
 
     # compute the content loss
     content_loss_y = tf.reduce_sum(

encoder.py
@@ -69,7 +69,10 @@ class Encoder(object):
         assert mode == "RGB"
         # preprocess
         if settings.config["IMAGE_SHAPE"][0] != 224 and "NO_SCALE" not in settings.config:
-            image = tf.image.resize(image, size=[224, 224])
+            if "pre_scale" in image.__dict__:
+                image = image.pre_scale
+            else:
+                image = tf.image.resize(image, size=[224, 224])
         
         # To BGR
         image = tf.reverse(image, axis=[-1])
@@ -83,9 +86,11 @@ class Encoder(object):
         image = tf.reverse(image, axis=[-1])
         image = tf.clip_by_value(image, 0.0, 255.0)
         
+        pre_scale = image
         if settings.config["IMAGE_SHAPE"][0] != 224 and "NO_SCALE" not in settings.config:
             image = tf.image.resize(
                 image, size=settings.config["IMAGE_SHAPE"][:2])
+        image.pre_scale = pre_scale
         return image
 
 def conv2d(x, kernel, bias):

utils.py
@@ -55,7 +55,6 @@ def average_gradients(tower_grads):
     return average_grads
 
 
-confidence = 50
 l2_weight = 1e-5
 
 def top_k_acc(logits,labels,k):
@@ -76,11 +75,11 @@ class build_logits:
         self.acc_y = tf.cast(tf.equal(self.prediction, label), tf.float32)
         self.acc = tf.reduce_mean(self.acc_y)
         self.logits = logits
-        self.label_logit = tf.reduce_sum(self.onehot_label*logits)
+        self.label_logit = tf.reduce_sum(self.onehot_label*logits, axis=-1)
         self.wrong_logit = tf.reduce_max(
-            (1-self.onehot_label)*logits - self.onehot_label*1e9)
-        self.target_loss = tf.nn.relu(
-            self.label_logit-self.wrong_logit+confidence)
+            (1-self.onehot_label)*logits - self.onehot_label*1e9, axis=-1)
+        self.target_loss = - tf.nn.relu(
+            self.label_logit-self.wrong_logit+conf)
         self.xent = tf.nn.sparse_softmax_cross_entropy_with_logits(
             labels=label, logits=logits)
         self.xent_sum = tf.reduce_sum(self.xent)
@@ -99,9 +98,11 @@ class build_logits:
             tf.reduce_sum(tf.nn.relu(self.true_logit5 - self.wrong_logit5 + conf), axis=1)
         if classes>50:
             self.accuracy = self.acc_5
+            self.acc_y_auto = self.acc_y_5 
             self.target_loss_auto = self.target_loss5
         else:
             self.accuracy = self.acc
+            self.acc_y_auto = self.acc_y
             self.target_loss_auto = self.target_loss
 
 def normalize(content,  epsilon=1e-5):